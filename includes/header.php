e6de5bd8903ec91fe53684b3e079c090ec449a55
HarryTate <harryjamestate@gmail.com>
2019-05-19 22:30:29 +0100

    restructured css to use a new sass layout, icons added and more areas added

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
+
+<head>
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Harry Tate - Junior Front End Web Developer</title>
+  <title>Harry Tate - Junior Front End Web Developer</title>
   <meta name="author" content="name">
   <meta name="description" content="description here">
   <meta name="keywords" content="Web Developer, web design, freelance websites">
+  <!--TODO gulp these together -->
   <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
-  <!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css"> -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css" type="text/css">
-  </head>
-  <body>
+</head>
+
+<body>
   <header class="header">
     <div class="header__logo">
       <span class="logo--text">Harry Tate</span>
