c89d602fd4b807713457dec67551c337a42232f2
Harry Tate <harry.tate@managers.org.uk>
2020-02-14 23:23:52 +0000

    Added cards, skills and hero home component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,9 @@
-import React from "react"
+import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="container">
-      <ul className="footer-social__list">
-        <li className="footer-social__item">
-          <a href="https://www.linkedin.com/in/harry-tate-0576a814b/">
-            LinkedIn
-          </a>
-        </li>
-        <li className="footer-social__item">
-          <a href="https://github.com/HarryTate">Github</a>
-        </li>
-        <li className="footer-social__item">
-          <a href="https://www.codepen.io/harrytate/">Codepen</a>
-        </li>
-      </ul>
-    </div>
+    <React.Fragment>
+      <p>This is the footer</p>
+    </React.Fragment>
   )
 }
--- src/pages/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <React.Fragment>
-      <p>This is the footer</p>
-    </React.Fragment>
-  )
-}
