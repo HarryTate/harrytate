21e30db8dc92f0f79efaeab1335113d6067b2c4c
Harry Tate <30785663+HarryTate@users.noreply.github.com>
2020-03-05 22:32:18 +0000

    Develop (#15)
    * Add image for PPL Projects
    
    * Added project showcase blocks
    
    * Minor amends to projects showcase for mobile responsiveness
    
    * Added project info work
    
    * Commmented out projectDetails for those that do not yet exist
    
    * Completed MVP on ProjectDetails Component including requirements and carousel images

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,44 +1,75 @@
 import React from "react"
 import ImageGallery from "react-image-gallery"
 
-const ProjectDetails = ({ images, name, client, techUsed, brief }) => {
+const ProjectDetails = ({
+  images,
+  name,
+  client,
+  techUsed,
+  brief,
+  date,
+  requirements,
+  siteLink,
+  repoLink,
+}) => {
   return (
-    <React.Fragment>
-      <section className="section">
-        <div className="container">
-          <div className="row">
-            <div className="half">
-              <div className="project-info">
-                <h2 className="project-info__title">Project Details</h2>
-                <dl className="project-info__data">
-                  <dt className="project-info__data-title">Project Name:</dt>
-                  <dd className="project-info__data-info">{name}</dd>
-                </dl>
-                <dl className="project-info__data">
-                  <dt className="project-info__data-title">Client:</dt>
-                  <dd className="project-info__data-info">{client}</dd>
-                </dl>
-                <dl className="project-info__data">
-                  <dt className="project-info__data-title">Tech:</dt>
-                  <dd className="project-info__data-info">
-                    {techUsed.map((tech, index) => (index ? ", " : "") + tech)}
-                  </dd>
-                </dl>
-              </div>
-              <h2>Project Brief</h2>
-              {brief}
+    <section className="section">
+      <div className="container">
+        <div className="row">
+          <div className="half order-tablet-portrait-up-second">
+            <ImageGallery
+              showPlayButton={false}
+              showFullscreenButton={false}
+              items={images}
+            />
+          </div>
+          <div className="half order-tablet-portrait-up-first">
+            <div className="project-info">
+              <dl className="project-info__data">
+                <dt className="project-info__data-title">Project Name:</dt>
+                <dd className="project-info__data-info">{name}</dd>
+              </dl>
+              <dl className="project-info__data">
+                <dt className="project-info__data-title">Client:</dt>
+                <dd className="project-info__data-info">{client}</dd>
+              </dl>
+              <dl className="project-info__data">
+                <dt className="project-info__data-title">Date:</dt>
+                <dd className="project-info__data-info">{date}</dd>
+              </dl>
+              <dl className="project-info__data">
+                <dt className="project-info__data-title">Tech:</dt>
+                <dd className="project-info__data-info">
+                  {techUsed.map((tech, index) => (index ? ", " : "") + tech)}
+                </dd>
+              </dl>
             </div>
-            <div className="half">
-              <ImageGallery
-                showPlayButton={false}
-                showFullscreenButton={false}
-                items={images}
-              />
+            <div className="project-content">
+              <h2 className="project-content__title">Project Brief</h2>
+              {brief}
+              <h2 className="project-content__title">Requirements</h2>
+              {requirements}
+              <a
+                href={siteLink}
+                className="btn btn__outline btn__outline--dark-blue mr--half"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View site
+              </a>
+              <a
+                href={repoLink}
+                className="btn btn__outline btn__outline--dark-blue"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Code
+              </a>
             </div>
           </div>
         </div>
-      </section>
-    </React.Fragment>
+      </div>
+    </section>
   )
 }
 
--- src/pages/projects/ppl-architects/index.js
+++ src/pages/projects/ppl-architects/index.js
@@ -5,6 +5,9 @@ import ProjectDetails from "../../../components/ProjectDetails"
 import Footer from "../../../components/Footer"
 import PPLMac from "../../../images/projects/PPL/Macbook.png"
 import PPLMacLaptop from "../../../images/projects/PPL/Macbook-laptop.png"
+import PPLHome from "../../../images/projects/PPL/home-full.png"
+import PPLContact from "../../../images/projects/PPL/contact-full.png"
+import PPLServices from "../../../images/projects/PPL/services-full.png"
 
 const PPLArchitects = props => {
   const images = [
@@ -17,8 +20,16 @@ const PPLArchitects = props => {
       thumbnail: PPLMacLaptop,
     },
     {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
+      original: PPLHome,
+      thumbnail: PPLHome,
+    },
+    {
+      original: PPLServices,
+      thumbnail: PPLServices,
+    },
+    {
+      original: PPLContact,
+      thumbnail: PPLContact,
     },
   ]
   return (
@@ -37,13 +48,28 @@ const PPLArchitects = props => {
           "Netlify",
           "Bootstrap",
         ]}
+        date={"January 2020"}
         brief={
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-            neque, ipsa nostrum labore quis amet dolorum magnam aut repudiandae
-            tempore.
+          <p className="project-content__text">
+            I was asked by a group of students at the University of Manchester
+            to create a basic website for a fictional architecture practice. My
+            job was to come up with a layout and design for the website and
+            deliver it in a tight deadline. Due to the short timeline, I used
+            the bootstrap framework for rapid development, parcel to bundle the
+            project and Netlify for a effortless deployment.
           </p>
         }
+        requirements={
+          <ul className="project-content__list">
+            <li>Mobile responsive</li>
+            <li>Email data capture contact form</li>
+            <li>Video home screen</li>
+            <li>Modern design</li>
+            <li>Full branding implementation</li>
+          </ul>
+        }
+        siteLink={"https://ppl-architects.netlify.com/"}
+        repoLink={"https://github.com/HarryTate/PeoplePlaceLifestyle"}
       />
       <Footer />
     </React.Fragment>
