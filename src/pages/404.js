163c814aeac5c0ee7eb2e4b7800fbd0c4ac10f09
Harry Tate <harryjamestate@gmail.com>
2020-03-06 19:26:48 +0000

    Added 404 header and footer and added helping minds showcase image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
-const pageNotFound = () => {
-  return <h1>Ooops: This is the 404</h1>
-}
-
+const pageNotFound = () => (
+  <React.Fragment>
+    <Header />
+    <h1>Ooops: This is the 404</h1>
+    <Footer />
+  </React.Fragment>
+)
 export default pageNotFound
