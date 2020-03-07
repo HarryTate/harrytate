3ebb2d86cdc471d4a34334c62cf733e193fcbb72
Harry Tate <30785663+HarryTate@users.noreply.github.com>
2020-03-07 22:28:49 +0000

    Develop (#20)
    * Added das images
    
    * Rename image
    
    * Added optional props for projectDetails Page

--- src/pages/projects/cmi/index.js
+++ src/pages/projects/cmi/index.js
@@ -1,10 +1,65 @@
 import React from "react"
+import Header from "../../../components/Header"
 import PageTitle from "../../../components/PageTitle"
+import ProjectDetails from "../../../components/ProjectDetails"
+import Footer from "../../../components/Footer"
 
 const cmi = () => {
+  const images = [
+    {
+      original: "https://placeimg.com/640/480/any",
+      thumbnail: "https://placeimg.com/640/480/any",
+    },
+    {
+      original: "https://placeimg.com/640/480/any",
+      thumbnail: "https://placeimg.com/640/480/any",
+    },
+    {
+      original: "https://placeimg.com/640/480/any",
+      thumbnail: "https://placeimg.com/640/480/any",
+    },
+  ]
+
   return (
     <React.Fragment>
-      <PageTitle color="red" title="CMI" />
+      <Header />
+      <PageTitle color="dark-grey" title="Helping Minds" />
+      <ProjectDetails
+        images={images}
+        name={"CMI"}
+        client={"Chartered Management Institute"}
+        techUsed={["HTML", "CSS", "JavaScript", "jQuery", "Sitecore CMS"]}
+        date={"March 2018 - Present"}
+        brief={
+          <p className="project-content__text">
+            I started working at the Chartered Management Institute as a Junior
+            Front-End Developer in 2018. My role is too build new pages on the
+            site and add new functionality for people around the business. These
+            usually include campaign, event and information pages.
+          </p>
+        }
+        requirements={
+          <ul className="project-content__list">
+            <li>Build bespoke campaign pages defined by the marketing team</li>
+            <li>
+              Maintain existing web pages making design changes that meet W3C
+              best practices
+            </li>
+            <li>Maintain SEO Standards</li>
+            <li>Support the business with new digital initiatives</li>
+            <li>
+              Build new components for users of the CMS around the buisness to
+              use
+            </li>
+          </ul>
+        }
+        featuredWork={[
+          { title: "Item 1", link: "https://www.managers.org.uk/campaigns" },
+          { title: "Item 2", link: "https://www.managers.org.uk/campaigns" },
+          { title: "Item 3", link: "https://www.managers.org.uk/campaigns" },
+        ]}
+      />
+      <Footer />
     </React.Fragment>
   )
 }
