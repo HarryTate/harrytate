50dff491890ee4406351be318c3a8858f61fc573
Harry Tate <30785663+HarryTate@users.noreply.github.com>
2020-03-13 11:50:49 +0000

    Develop (#29)
    * Added twitter meta data
    
    * Intro reword
    
    * Added SEO component to pages
    
    * Text change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,14 +9,14 @@ export default class HeroHome extends Component {
           <div className="intro__content">
             <p className="intro__content-text">
               Hi, I'm Harry and I'm passionate about building responsive, user
-              friendly websites. I'm currently working at the the&nbsp;
+              friendly websites. I'm currently working at the&nbsp;
               <a
                 className="text--bold"
                 href="https://www.managers.org.uk"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Chartered Management Institue
+                Chartered Management Institute
               </a>
               &nbsp;as a Front-End Developer. In 2017 I completed a degree in
               Computing BSc at Nottingham Trent University.
