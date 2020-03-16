4a055ebe82d96d737965f9bb65e9220064d5cd9a
Harry Tate <harryjamestate@gmail.com>
2020-03-16 23:48:00 +0000

    Added social icons into there own component and added to footer fixed #31

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Social from "../components/Social"
 
 const Footer = () => {
   return (
@@ -30,19 +31,7 @@ const Footer = () => {
           </div>
           <div className="footer-content">
             <h3 className="footer-content__heading">Social</h3>
-            <ul className="footer-social__list">
-              <li className="footer-social__item">
-                <a href="https://www.linkedin.com/in/harry-tate-0576a814b/">
-                  LinkedIn
-                </a>
-              </li>
-              <li className="footer-social__item">
-                <a href="https://github.com/HarryTate">Github</a>
-              </li>
-              <li className="footer-social__item">
-                <a href="https://www.codepen.io/harrytate/">Codepen</a>
-              </li>
-            </ul>
+            <Social />
           </div>
           <div className="footer-content">
             <h3 className="footer-content__heading">Contact</h3>
