ce275a2e2134d4716f69d23557f972463cff4a68
Harry Tate <harryjamestate@gmail.com>
2020-03-16 22:34:14 +0000

    Removed svg fixed #35

--- src/components/HeroHome.js
+++ src/components/HeroHome.js
@@ -3,27 +3,13 @@ import { Link } from "gatsby"
 import "../styles/main.scss"
 
 const HeroHome = props => (
-  <React.Fragment>
-    <section className="hero-home">
-      <h1 className="hero-home__heading">{props.name}</h1>
-      <p className="hero-home__subtitle">{props.content}</p>
-      <Link className="btn btn--red" to="/about/">
-        Find out more about me
-      </Link>
-    </section>
-    <svg
-      className="hero__wave"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1440 140"
-    >
-      <path
-        preserveAspectRatio="xMidYMid none"
-        fill="#3284e2"
-        fillOpacity="1"
-        d="M0,128L120,117.3C240,107,480,85,720,85.3C960,85,1200,107,1320,117.3L1440,128L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
-      ></path>
-    </svg>
-  </React.Fragment>
+  <section className="hero-home">
+    <h1 className="hero-home__heading">{props.name}</h1>
+    <p className="hero-home__subtitle">{props.content}</p>
+    <Link className="btn btn--red" to="/about/">
+      Find out more about me
+    </Link>
+  </section>
 )
 
 export default HeroHome
